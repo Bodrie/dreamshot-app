23d14ad3d17d3d62f85b478ceeed2a46e87280d4
Dobromir Kirov <kirov0407@gmail.com>
2023-06-10 12:58:20 +0300

    feat: added sass, created initial style variable file, imported initial font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import styles from "./App.module.scss";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const { App } = styles;
+  return <div className={App}></div>;
 }
 
 export default App;
